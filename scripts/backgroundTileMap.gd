0a6797e3851697a1ff637f2d38572bd5c16f7882
onyx <info@jeremy.best>
2023-02-04 01:57:57 +0100

    correct noise map for spawn

--- scripts/backgroundTileMap.gd
+++ scripts/backgroundTileMap.gd
@@ -3,15 +3,22 @@ extends Node2D
 var current_level = 1
 var map_set 
 
-var noise = OpenSimplexNoise.new()
+var noise_collectable = OpenSimplexNoise.new()
+var noise_rock = OpenSimplexNoise.new()
 onready var Collectable = load("res://scenes/collectable.tscn")
+onready var Rock = load("res://scenes/obstacle.tscn")
 
 func _ready():
 	# Configure
-	noise.seed = randi()
-	noise.octaves = 4
-	noise.period = 20.0
-	noise.persistence = 0.8
+	noise_collectable.seed = randi()
+	noise_collectable.octaves = 4
+	noise_collectable.period = 20.0
+	noise_collectable.persistence = 0.8
+	
+	noise_rock.seed = randi()
+	noise_rock.octaves = 4
+	noise_rock.period = 20.0
+	noise_rock.persistence = 0.8
 #	set_tileset_map()
 	var texture = create_texture("res://assets/tiile_base.png")
 	var tilemap = load_image_to_tilemap(texture, Vector2(1000, 2000))
@@ -29,24 +36,25 @@ func create_texture(path_img):
 
 func load_image_to_tilemap(texture, size):
 	var tilemap = TileMap.new()
-	var collect_tmp
+	var tmp_instance
 	tilemap.set_tileset(texture)
 	tilemap.set_cell_size(texture.autotile_get_size(1))
 	
 	# Boucle pour remplir le tileMap avec la taille définie
 	for x in range(0, size.x, texture.autotile_get_size(1).x):
 		for y in range(0, size.y, texture.autotile_get_size(1).y):
-			
-			
-			for x_x in range(0, x, 10):
-				for y_y in range(0, y, 10):
-					var noise_res = noise.get_noise_2d(x_x, y_y)
-					if (noise_res >= 0.6):
-						collect_tmp = Collectable.instance()
-						collect_tmp.set_position(Vector2(x_x, y_y))
-						add_child(collect_tmp)
-						x_x += 2000
-						y_y += 2000
 			tilemap.set_cell(x / texture.autotile_get_size(1).x, y / texture.autotile_get_size(1).y, 1)
+			
+	for x in range(0, size.x, 10):
+		for y in range(0, size.y, 10):
+			if (noise_collectable.get_noise_2d(x, y) >= 0.5):
+				tmp_instance = Collectable.instance()
+				tmp_instance.set_position(Vector2(x, y))
+				add_child(tmp_instance)
+			elif (noise_rock.get_noise_2d(x, y) >= 0.47):
+				tmp_instance = Rock.instance()
+				tmp_instance.set_position(Vector2(x, y))
+				add_child(tmp_instance)
+
 	return tilemap
 
