96d3fafd5d96977085f7144678079b620d703af4
znichola <niki.zivkovic@hotmail.com>
2023-02-04 14:20:34 +0100

    merge rush

--- scripts/root.gd
+++ scripts/root.gd
@@ -3,6 +3,9 @@ extends KinematicBody2D
 export (int) var speed = 100
 export (float) var rotation_speed = 1.8
 
+onready var path_line = $N/Path
+onready var path_timer = $N/PathPointTimer
+
 var velocity = Vector2()
 var rotation_dir = 0
 var rot_limit = PI/16 # in radians, PI is 180 deg
@@ -22,11 +25,14 @@ func _physics_process(delta):
 	rotation += rotation_dir * rotation_speed * delta
 	rotation = clamp(rotation, -PI+rot_limit, -rot_limit)
 	velocity = move_and_slide(velocity)
-	$N/Path.set_point_position($N/Path.get_point_count(), position)
+	var points_count = path_line.points.size()
+	if (points_count > 1):
+		path_line.set_point_position(points_count - 1, position)
 	
 func _on_PathPointTimer_timeout():
-#	print("appended", position)
-	$N/Path.add_point(position)
-#	$N/Path.set_draw_behind_parent(true)
-#	$RootHead._set_on_top(true)
-	$N/PathPointTimer.start()
+	path_line.add_point(position)
+	path_timer.start()
+
+
+func _on_Timer_timeout():
+	print("we should die")
