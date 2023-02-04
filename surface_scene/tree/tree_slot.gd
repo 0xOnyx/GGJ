e39dacb9ea47d0f6c4a33bd673e477be9ce7c6f6
JordanMAC <contact@jordan-chap.com>
2023-02-04 19:39:54 +0100

    del_usless

--- surface_scene/tree/tree_slot.gd
+++
@@ -1,9 +0,0 @@
-extends Node2D
-
-onready var tree_menu = preload("res://scenes/store/store_main.tscn")
-
-func _on_slot_area_input_event(viewport, event, shape_idx):
-	if event is InputEventMouseButton and get_tree().get_node_count() == 4:
-		var menu_instance = tree_menu.instance()
-		menu_instance.position = get_viewport().get_mouse_position()
-		add_child(menu_instance)
