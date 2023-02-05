583dcd9d4021e5a148ec1cc0f8d76cd837e72267
onyx <info@jeremy.best>
2023-02-05 12:42:57 +0100

    add nickname #40

--- scenes/menu/menu_script/menu_start.gd
+++ scenes/menu/menu_script/menu_start.gd
@@ -3,6 +3,7 @@ extends Node2D
 func _on_but_start_pressed():
 	$son.play()
 	get_tree().change_scene("res://scenes/main.tscn") #Pour passer a la scene principale de jeu
+	g.nickname = $LineEdit.get_text()
 
 
 func _on_but_option_pressed():
@@ -11,4 +12,4 @@ func _on_but_option_pressed():
 		$anim.play("show_hide_option")
 	else:
 		$anim.play_backwards("show_hide_option")
-		
+
